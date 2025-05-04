79504bcd607f68ad4d9ca3dbc846bea6c4a1db17
koiyae <bruno.ryan.bs@gmail.com>
2025-05-04 11:21:31 -0300

    feat: lexical analysis

--- lexer.py
+++ lexer.py
@@ -14,9 +14,13 @@ class Lexer:
         return ch
     
     def peek(self):
+        if self.curr >= len(self.source):
+            return '\0'
         return self.source[self.curr]
 
     def lookahead(self, n=1):
+        if self.curr >= len(self.source):
+            return '\0'
         return self.source[self.curr + n]
 
     def match(self, expected):
@@ -24,8 +28,38 @@ class Lexer:
             return False
         if self.source[self.curr] != expected:
             return False
-        self.curr = self.curr + 1
+        self.curr += 1
         return True
+
+    def handle_number(self):
+        while self.peek().isdigit():
+            self.advance()
+        if self.peek() == '.' and self.lookahead().isdigit():
+            self.advance()
+            while self.peek().isdigit():
+                self.advance()
+            self.add_token(TOK_FLOAT)
+        else:
+            self.add_token(TOK_INTEGER)
+
+    def handle_string(self, start_quote):
+        while self.peek() != start_quote and not(self.curr >= len(self.source)):
+            self.advance()
+        if self.curr >= len(self.source):
+            raise SyntaxError(f'[Line {self.line}] unterminated string.')
+        self.advance()
+        self.add_token(TOK_STRING)
+
+    def handle_identifier(self):
+        while self.peek().isalnum() or self.peek() == '_':
+            self.advance()
+
+        text = self.source[self.start:self.curr]
+        keyword_type = keywords.get(text)
+        if keyword_type == None:
+            self.add_token(TOK_IDENTIFIER)
+        else:
+            self.add_token(keyword_type)
 
     def add_token(self, token_type):
         self.tokens.append(Token(token_type, self.source[self.start:self.curr], self.line)) 
@@ -39,11 +73,6 @@ class Lexer:
             elif ch == ' ': pass
             elif ch == '\t': pass
             elif ch == '\r': pass
-
-            elif ch == '#':
-                while self.peek() != '\n':
-                    self.advance()
-
             elif ch == '(': self.add_token(TOK_LPAREN)
             elif ch == ')': self.add_token(TOK_RPAREN)
             elif ch == '{': self.add_token(TOK_LCURLY)
@@ -53,13 +82,47 @@ class Lexer:
             elif ch == '.': self.add_token(TOK_DOT)
             elif ch == ',': self.add_token(TOK_COMMA)
             elif ch == '+': self.add_token(TOK_PLUS)
-            elif ch == '-': self.add_token(TOK_MINUS)
+            elif ch == '-':
+                if self.match('-'):
+                    while self.peek() != '\n' and not(self.curr >= len(self.source)):
+                        self.advance()
+                else:
+                    self.add_token(TOK_MINUS)
             elif ch == '*': self.add_token(TOK_STAR)
             elif ch == '^': self.add_token(TOK_CARET)
             elif ch == '/': self.add_token(TOK_SLASH)
             elif ch == ';': self.add_token(TOK_SEMICOLON)
             elif ch == '?': self.add_token(TOK_QUESTION)
             elif ch == '%': self.add_token(TOK_MOD)
+            elif ch  == '=':
+                if self.match('='):
+                    self.add_token(TOK_EQ)
+            elif ch == '~':
+                if self.match('='):
+                    self.add_token(TOK_NE)
+                else:
+                    self.add_token(TOK_NOT)
+            elif ch == '<':
+                if self.match('='):
+                    self.add_token(TOK_LE)
+                else:
+                    self.add_token(TOK_LT)
+            elif ch == '>': 
+                if self.match('='):
+                    self.add_token(TOK_GE)
+                else:
+                    self.add_token(TOK_GT)
+            elif ch == ':':
+                if self.match('='):
+                    self.add_token(TOK_ASSIGN)
+                else:
+                    self.add_token(TOK_COLON)
+            elif ch.isdigit():
+                self.handle_number()
+            elif ch == '"' or ch == '\'':
+                self.handle_string(ch)
+            elif ch.isalpha() or ch == '_':
+                self.handle_identifier()
         return self.tokens
 
 
